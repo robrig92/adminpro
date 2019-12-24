e882003be9871d7f4d413b7f31e5d36a85acb105
Roberto Padilla <robert.ipo@hotmail.com>
2019-12-23 22:02:36 -0600

    Modularizando la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,12 @@ import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
     RegisterComponent
   ],
   imports: [
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
 
 @NgModule({
     declarations: [
@@ -16,6 +18,10 @@ import { PagesComponent } from './pages.component';
         ProgressComponent,
         Graficas1Component,
         PagesComponent
+    ],
+    imports: [
+    SharedModule,
+        PAGES_ROUTES
     ]
 })
 export class PagesModule {}
